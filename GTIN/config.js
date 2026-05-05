2e2975ba4163be77f9b68ee9693a62e7d9213bb3
CP44707 <colby@covertclan.com>
2026-05-05 10:14:11 -0600

    Revert "Update scanner.html"
    This reverts commit d6669b9d74a812034a120dfaceeb2446bbf30761.
    
    Reapply "Update scanner.html"
    
    This reverts commit 12f958fa82fbce8cc03f9f9541f4ce8fb23c2d42.
    
    Revert "Update scanner.html"
    
    This reverts commit 02c5d8033d9fa2437ae1c5d6f34c531a616be2d4.
    
    Revert "Update scanner.html"
    
    This reverts commit 5adc2df2be40727b4a5ec6338dd8c34bfbef81bc.
    
    Revert "Update scanner.html"
    
    This reverts commit a699d1eca4048ac60e78f162460b9c959a34c5b9.
    
    Revert "Update scanner.html"
    
    This reverts commit d097e2211c2210e7d232ae0184c83d8dd951ab5a.
    
    Revert "Update scanner.html"
    
    This reverts commit d7649e7a480644d8ab311482a98dd4f77883d931.
    
    Revert "Update scanner.html"
    
    This reverts commit b943e588c3f4b55f6a934f6299ab78e25ff523ee.
    
    Revert "Update scanner.html"
    
    This reverts commit bf9ce3b918795b5907e959bf502403880510cf9c.
    
    Revert "Added Receiving"
    
    This reverts commit 4152fe5edce3b388c8d70e141b0fe5a79fcdfaf4.
    
    Revert "Update review.html"
    
    This reverts commit d3d4e24d3724517dee767de258c09d5f6863a384.
    
    Revert "Update review.html"
    
    This reverts commit a4bbdb96398aca4cbcc4bb9d454c31f47f272501.
    
    Revert "Update config.js"
    
    This reverts commit cd542012e63cfa0bbd98e2f6e3c54bb6947d7721.
    
    Revert "Fixing Date and display"
    
    This reverts commit 310d15010fd9c964fec6e5b25d8db458bb143ec3.
    
    Revert "Added Cycle review"
    
    This reverts commit 2ea3e4402d768b9a52bf6d138d9b3ef2d5efb0b4.

--- GTIN/config.js
+++ GTIN/config.js
@@ -1,8 +1,5 @@
 const CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbzeQtG3p4ATqX6Um5WeyvypH7RpPYkAVOxm5F_sLzav1UnDeJu_D81naEMU0mTT19-C/exec",
-  CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQcNmdR8CzUL7BKaZkdrUttj8u9z73q6ThlT2h_6c5ua_dtbb6D93O-80vFDR3OwS-OcdXH_2NWhikf/pub?output=csv",
-  CYCLE_URL: "https://script.google.com/macros/s/AKfycbzzujRdB2K3bbXUh4jq-ePIE7BpaPznvL-Al7WEAzqpvtG2HAcRU8n2BvreN6yB7NZQsQ/exec",
-  RECEIVE_URL: "https://script.google.com/macros/s/AKfycbwgXASrYocJT51hLX2YlVlLTpvsRwJwJVtOvd4k1qxaWkA_O1D7kS4x_UQqvpIEVK0/exec"
-
+  CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQcNmdR8CzUL7BKaZkdrUttj8u9z73q6ThlT2h_6c5ua_dtbb6D93O-80vFDR3OwS-OcdXH_2NWhikf/pub?output=csv"
 };
 

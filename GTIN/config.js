760997a3d923811c1cddd817793ffce201268703
CP44707 <colby@covertclan.com>
2026-05-07 15:25:46 -0600

    Pick has sheet

--- GTIN/config.js
+++ GTIN/config.js
@@ -3,7 +3,9 @@ const CONFIG = {
   CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQcNmdR8CzUL7BKaZkdrUttj8u9z73q6ThlT2h_6c5ua_dtbb6D93O-80vFDR3OwS-OcdXH_2NWhikf/pub?output=csv",
   CYCLE_URL: "https://script.google.com/macros/s/AKfycbzzujRdB2K3bbXUh4jq-ePIE7BpaPznvL-Al7WEAzqpvtG2HAcRU8n2BvreN6yB7NZQsQ/exec",
   RECEIVE_URL: "https://script.google.com/macros/s/AKfycbwgXASrYocJT51hLX2YlVlLTpvsRwJwJVtOvd4k1qxaWkA_O1D7kS4x_UQqvpIEVK0/exec",
-  RECEIVE_CSV: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR8_SEEMLGeKrYaH0kxTaQLNZxhrDVB6tBSWpY4jSrq-mjaLkOFodq_RizYbESt5FOseq_4Y7goMsxz/pub?output=csv"
+  RECEIVE_CSV: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR8_SEEMLGeKrYaH0kxTaQLNZxhrDVB6tBSWpY4jSrq-mjaLkOFodq_RizYbESt5FOseq_4Y7goMsxz/pub?output=csv",
+  PICK_URL: "https://script.google.com/macros/s/AKfycbypDzmmPWBqROlvl35P7iSGHkJlIzFL3Wckm5YDrgiSX233XAXKBN7z7U8IqD2HPvM/exec",
+  PICK_CSV: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRU-2e5EOJ29osqITyaqrTQ5kVHt0RBUYzDQNfJOeGWP_4KQgnBHE9lAPAtmgsOfRAO2qXOs4c4CtRm/pub?output=csv"
 
 };
 
